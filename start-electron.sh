d912feea05450a509c8885b2e84630cfc7237e41
RMateosAyesa <rmateos@emergya.com>
2025-05-08 23:24:34 +0200

    akshdba

--- start-electron.sh
+++ start-electron.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ELECTRON_DISABLE_SANDBOX=true \
+export ELECTRON_DISABLE_SANDBOX=true
+export DISPLAY=:99
+
 xvfb-run --auto-servernum --server-args='-screen 0 1280x720x24' \
 ./app/DevaidStarter_Dev-4.1.1.AppImage --no-sandbox
