64808b4c84369af094d69b65996753920e981d6c
RMateosAyesa <rmateos@emergya.com>
2025-05-08 23:04:29 +0200

    askhjdnak

--- start-electron.sh
+++ start-electron.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Wrapper para lanzar tu AppImage bajo un entorno X virtual (xvfb)
 
-ELECTRON_DISABLE_SANDBOX=true xvfb-run --auto-servernum --server-args='-screen 0 1280x720x24' ./app/DevaidStarter_Dev-4.1.1.AppImage --no-sandbox
+ELECTRON_DISABLE_SANDBOX=true \
+xvfb-run --auto-servernum --server-args='-screen 0 1280x720x24' \
+./app/DevaidStarter_Dev-4.1.1.AppImage --no-sandbox
