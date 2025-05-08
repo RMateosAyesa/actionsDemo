824903c3ccf25f3cf3508d7e947159802aa8ea6a
RMateosAyesa <rmateos@emergya.com>
2025-05-08 23:45:03 +0200

    mas cosas

--- start-electron.sh
+++ start-electron.sh
@@ -4,4 +4,5 @@ export ELECTRON_DISABLE_SANDBOX=true
 export DISPLAY=:99
 
 xvfb-run --auto-servernum --server-args='-screen 0 1280x720x24' \
-./app/DevaidStarter_Dev-4.1.1.AppImage --no-sandbox
+./app/DevaidStarter_Dev-4.1.1.AppImage --no-sandbox --disable-gpu --disable-software-rasterizer
+
