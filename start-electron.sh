325133fd2627c4e1e48d7446c8d5efaa400b5b52
RMateosAyesa <rmateos@emergya.com>
2025-05-09 00:18:52 +0200

    akshdbajhsd

--- start-electron.sh
+++ start-electron.sh
@@ -2,13 +2,15 @@
 set -e
 
 export ELECTRON_DISABLE_SANDBOX=true
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/tmp/dbus-fake
 export DISPLAY=:99
+
+eval `dbus-launch --sh-syntax`
+export DBUS_SESSION_BUS_ADDRESS
+export DBUS_SESSION_BUS_PID
 
 xvfb-run --auto-servernum --server-args='-screen 0 1280x720x24' \
 ./app/DevaidStarter_Dev-4.1.1.AppImage \
 --no-sandbox \
 --disable-gpu \
 --disable-software-rasterizer \
---disable-dev-shm-usage \
---no-startup-window
+--disable-dev-shm-usage
