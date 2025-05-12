552a27bda8d75b4c1f54102546b4783af9b16a22
RMateosAyesa <rmateos@emergya.com>
2025-05-13 00:43:42 +0200

    ninoniiii

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     appImage: './app/DevaidStarter_Dev-4.1.1.AppImage',
-    args: ['--no-sandbox', '--disable-gpu'],
+    args: ['--no-sandbox', '--disable-gpu', '--disable-software-rasterizer', '--disable-dev-shm-usage', '--disable-sandbox'],
   };
   
