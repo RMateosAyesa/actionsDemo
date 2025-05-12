9dabb325b32eb4788606d4f8b46e39b0599c49c7
RMateosAyesa <rmateos@emergya.com>
2025-05-13 00:31:55 +0200

    aksujdnhaksd

--- config.js
+++ config.js
@@ -1,22 +1,5 @@
-const path = require('path');
-
 module.exports = {
-  use: {
-    // Otros settings
-  },
-  projects: [
-    {
-      name: 'electron',
-      testMatch: /.*\.spec\.js/,
-      use: {
-        // Playwright config for Electron
-        launchOptions: {
-          appImage: path.resolve(__dirname, './app/DevaidStarter_Dev-4.1.1.AppImage'),
-          env: {
-            DISPLAY: process.env.DISPLAY,
-          },
-        },
-      },
-    },
-  ],
-};
+    appImage: './app/DevaidStarter_Dev-4.1.1.AppImage',
+    args: ['--no-sandbox', '--disable-gpu'],
+  };
+  
